05588ad177a245ba29e62c639c07ee07d1a1ee82
Kirill Churkin <k.churkin@bi.zone>
2020-12-10 17:06:58 +0300

    fix division by zero

--- script.py
+++ script.py
@@ -4,6 +4,9 @@ operation = input()
 if operation() == "+":
     print(a + b)
 elif operation == "%" or operation == "mod":
+    if b == 0:
+        print("Division by zero")
+        exit(1)
     print(a % b)
 elif operation == "-":
     print(a - b)
